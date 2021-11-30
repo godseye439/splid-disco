aa767ad398f2ca97c4d972ce09da99d4b23133fc
godseye439 <44599410+godseye439@users.noreply.github.com>
2021-11-30 20:41:52 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Renamer-Bot
+## Mapping
+[Deploy to Heroku 🗳](https://github.com/Ns-Bots/TG-RENAMER-BOT#deploy-to-heroku-)
+## Available Commands 🤖
+```
+start - check whether bot is alive or not
+mode - change video upload mode to file or video
+showthumbnail - to check permanent thumbnail
+deletethumbnail - to delete the permanent thumbnail
+help - know how to use the bot
+about - know about the bot
+reset - (admins only) reset users database
+login - (when password is given) to login to bot
+```
---
+++ renamer/tools/text.py
@@ -0,0 +1,38 @@
+from pyrogram.emoji import *
+
+class TEXT:
+    DOWNLOAD_START = f"Trying to Download the file {SLEEPING_FACE}"
+    UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
+    UPLOAD_SUCESS = f"Rename Sucessfull"
+    BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
+    NOT_LOGGED_TEXT = f"This bot was only for @nflix_movies {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail Deleted Successfully {CHECK_MARK_BUTTON}"
+    NO_CUSTOM_THUMB_NAIL_FOUND = f"𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 {THUMBS_DOWN_LIGHT_SKIN_TONE}"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+
+
+    ABOUT = """**𝖬𝗒 𝖣𝖾𝗍𝖺𝗂𝗅𝗌 :**
+
+Nothing will be disclosed to unautorized users 
+"""
+
+    HELP_USER = """**Follow Below Steps:**
+   
+☞︎︎︎ Use /mode command to change upload mode.
+☞︎︎︎ Send a photo to set as permanent thumbnail.
+☞︎︎︎ Now send me the Telegram file you want to rename.
+☞︎︎︎ Send the new name when bot ask.
+
+For source code check about
+"""
+
+    START_TEXT = """Hi {user_mention},
+
+I'm a bot made & working for @nflix_movies. Unauthorized users can't use me.
+
+**Maintained By:** @nflix_movies
+"""
+
+
+    DONATE_USER = """NULL"""
